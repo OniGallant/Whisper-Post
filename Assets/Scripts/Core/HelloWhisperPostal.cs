bcfaebfc443170bc50285429587c926f0a0d45c6
Oni Gallant Studio <onigallantdev@gmail.com>
2025-09-11 20:58:02 +0530

    Initialize Commit
    Project setup + HelloScene test

---
+++ Assets/Scripts/Core/HelloWhisperPostal.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class HelloWhisperPost : MonoBehaviour
+{
+    // Start() runs once when the GameObject becomes active in the scene.
+    void Start()
+    {
+        Debug.Log("Hello, WhisperPost! Project and scripts are working.");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Whisper-Post
 A relaxing 2D game with emotions of humans.
+
+
